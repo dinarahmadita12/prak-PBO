36e3a41aa82a555436c1a90b8b714124f1ae874f
dinarahmadita12 <145269715+dinarahmadita12@users.noreply.github.com>
2024-03-02 19:25:06 +0700

    Update 122140184_DINA RAHMA DITA_PRAK 1_SOAL 1.py

--- prak1/122140184_DINA RAHMA DITA_PRAK 1_SOAL 1.py
+++ prak1/122140184_DINA RAHMA DITA_PRAK 1_SOAL 1.py
@@ -1,1 +1,15 @@
+batas_bawah = int(input("Batas bawah: "))
+batas_atas = int(input("Batas atas: "))
 
+def bil_ganjil(batas_bawah, batas_atas):
+    if batas_bawah < 0 or batas_atas < 0:
+        print("Batas bawah dan atas yang dimasukkan tidak boleh di bawah Nol")
+        return bil_ganjil
+
+    total_bil_ganjil = 0
+    for angka in range(batas_bawah, batas_atas):
+        if angka % 2 != 0:
+            total_bil_ganjil += angka
+            print(angka)
+    print(f"total : {total_bil_ganjil}")
+bil_ganjil(batas_bawah, batas_atas)
