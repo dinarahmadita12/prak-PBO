e42335918bda2e9e7b85895f5c1a9954e315d8d3
dinarahmadita12 <145269715+dinarahmadita12@users.noreply.github.com>
2024-03-02 19:25:29 +0700

    Update 122140184_DINA RAHMA DITA_PRAK 1_SOAL 2.py

--- prak1/122140184_DINA RAHMA DITA_PRAK 1_SOAL 2.py
+++ prak1/122140184_DINA RAHMA DITA_PRAK 1_SOAL 2.py
@@ -1,1 +1,19 @@
+class Lingkaran:
+    def __init__(self, rasio):
+        self.rasio = rasio
+    
+    def luas(self):
+        return self.rasio ** 2 * 3.14
+    
+    def keliling(self):
+        return 2 * 3.14 * self.rasio
+
+rasio = float(input("Masukkan rasio: "))
+
+if rasio < 0:
+    print("Jari-jari lingkaran tidak boleh negatif")
+else:
+    lingkaran = Lingkaran(rasio)
+    print("Luas: ", lingkaran.luas())
+    print("Keliling: ", lingkaran.keliling())
 
